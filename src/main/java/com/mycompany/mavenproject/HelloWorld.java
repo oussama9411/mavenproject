2eebf859a18605a71b9fe23a1a2e51c094bcb16b
unknown <oussama.hermessi.professional@gmail.com>
2019-03-11 00:08:16 +0100

    commit app

--- src/main/java/com/mycompany/mavenproject/HelloWorld.java
+++ src/main/java/com/mycompany/mavenproject/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
     
     public static void main(String[] args) {
         
-        System.out.println("hello oussama ");
+        System.out.println("hello world ");
         
     }
     
